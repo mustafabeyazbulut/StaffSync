4c9f66e875700028def2e1167874eb5d73dba74c
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2024-01-07 14:12:15 +0300

    .

--- StaffSync/Infrastructure/StaffSync.Persistence/obj/Debug/net8.0/StaffSync.Persistence.AssemblyInfo.cs
+++ StaffSync/Infrastructure/StaffSync.Persistence/obj/Debug/net8.0/StaffSync.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaffSync.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68cc5251494c5ac61ec9086e9018aec2d6d07537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaffSync.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaffSync.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
