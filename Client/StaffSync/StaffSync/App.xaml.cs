d495db0c3e98e01a5ffad2d2cf44036ecb9c4bfc
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2024-08-14 11:16:29 +0300

    StaffSync Front End Projesi oluşturuldu
    StaffSync Front End Projesi oluşturuldu

---
+++ Client/StaffSync/StaffSync/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StaffSync
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
